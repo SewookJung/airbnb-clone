8f54ae9ff727c160ba6df2f87255025aeb0c2227
sewookJung <tpdnrqkqh@naver.com>
2021-08-02 22:49:08 +0900

    [MODIFY] Auto now option in columns

--- core/models.py
+++ core/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class TimeStampedModel(models.Model):
     """Time Stamped"""
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
+        # This option is not migration in database
         abstract = True
