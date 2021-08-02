22e402193122170565529877e4e6a9ef4d2412ce
sewookJung <tpdnrqkqh@naver.com>
2021-08-03 00:00:42 +0900

    [ADD] Register Photo model in admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -3,10 +3,21 @@ from . import models
 
 
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
-class RoomTypeAdmin(admin.ModelAdmin):
+class ItemAdmin(admin.ModelAdmin):
+    """Item Admin Definition"""
+
     pass
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+    """Room Admin Definition"""
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """Photo Admin Definition"""
+
     pass
