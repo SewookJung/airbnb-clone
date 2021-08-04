483467aec28e4b75b7d0afb1113f9092cbf85b3e
sewookJung <tpdnrqkqh@naver.com>
2021-08-04 15:27:22 +0900

    [ADD] Used by column of Roomtype, Facility, Amenity, HouseRule

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,6 +6,11 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
     """Item Admin Definition"""
 
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -75,6 +80,7 @@ class RoomAdmin(admin.ModelAdmin):
         "instant_book",
         "host",
         "count_amenities",
+        "count_photos",
     )
 
     list_filter = (
@@ -96,7 +102,9 @@ class RoomAdmin(admin.ModelAdmin):
 
     def count_amenities(self, obj):
         return obj.amenities.count()
-        
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
