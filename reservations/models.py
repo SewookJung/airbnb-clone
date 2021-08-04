d0981220705221142c5f31be812235ee4edafe93
sewookJung <tpdnrqkqh@naver.com>
2021-08-04 17:43:53 +0900

    [ADD] In progress and finished function to added column in admin panel

--- reservations/models.py
+++ reservations/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from core import models as core_models
 
 
@@ -15,11 +16,29 @@ class Reservation(core_models.TimeStampedModel):
         (STATUS_CANCELED, "Canceled"),
     )
 
-    status = models.CharField(choices=STATUS_CHOICES, max_length=12, default=STATUS_PENDING)
+    status = models.CharField(
+        choices=STATUS_CHOICES, max_length=12, default=STATUS_PENDING
+    )
     check_in = models.DateField()
     check_out = models.DateField()
-    guest = models.ForeignKey("users.User", related_name="reservations", on_delete=models.CASCADE)
-    room = models.ForeignKey("rooms.Room", related_name="reservations", on_delete=models.CASCADE)
+    guest = models.ForeignKey(
+        "users.User", related_name="reservations", on_delete=models.CASCADE
+    )
+    room = models.ForeignKey(
+        "rooms.Room", related_name="reservations", on_delete=models.CASCADE
+    )
 
     def __str__(self):
-        return f'{self.room} - {self.check_in}'
+        return f"{self.room} - {self.check_in}"
+
+    def in_progress(self):
+        now = timezone.localtime().date()
+        return now >= self.check_in and now <= self.check_out
+
+    in_progress.boolean = True
+
+    def is_finished(self):
+        now = timezone.localtime().date()
+        return now > self.check_out
+
+    is_finished.boolean = True
