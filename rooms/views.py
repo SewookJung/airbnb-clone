007f8ebf9b72f5cb7ad9e2a7cfe19d751ea27f53
sewookJung <tpdnrqkqh@naver.com>
2021-08-11 15:26:17 +0900

    [ADD] Render all rooms with template and context

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def all_rooms(request):
-    pass
+    now = datetime.now()
+    return render(request, "all_rooms.html", context={"now": now})
