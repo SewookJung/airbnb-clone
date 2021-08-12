1ce588bfa3781942981119262f357c90caa8853d
sewookJung <tpdnrqkqh@naver.com>
2021-08-12 17:44:36 +0900

    [REMOVE] Un used annotation line

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls", namespace="core")),
-    # path("", include("core.urls", namespace="core")),
 ]
 
 if settings.DEBUG:
