f557d0e1e948c1faa1fc498d79f90e83c9264dae
sewookJung <tpdnrqkqh@naver.com>
2021-08-13 23:29:19 +0900

    [ADD] Rooms app url path

--- config/urls.py
+++ config/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", include("core.urls", namespace="core")),
+    path("rooms/", include("rooms.urls", namespace="rooms")),
+    path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
