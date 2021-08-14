51d6696e9480065fa88c30f85d6bfc6328121f75
sewookJung <tpdnrqkqh@naver.com>
2021-08-14 23:56:17 +0900

    [MODIFY] Change the template name by Detailview

--- config/settings.py
+++ config/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i^+#+2n*ue#&^23)t8=ym%w^s4^*@^^6i68j@@n8jmtyr8of@0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
