d24b3755530dcb5798fd801c5a361f5c850c01d0
sewookJung <tpdnrqkqh@naver.com>
2021-08-16 23:04:38 +0900

    [ADD] Room's detail informations

--- rooms/views.py
+++ rooms/views.py
@@ -23,11 +23,35 @@ class RoomDetail(DetailView):
 def search(request):
     city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    room_types = models.RoomType.objects.all()
     country = request.GET.get("country", "KR")
     room_type = int(request.GET.get("room-type", 0))
 
-    form = {"city": city, "s_room_type": room_type, "s_country": country}
-    choices = {"countries": countries, "room_types": room_types}
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    beds = int(request.GET.get("beds", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    baths = int(request.GET.get("baths", 0))
+
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "price": price,
+        "guests": guests,
+        "beds": beds,
+        "bedrooms": bedrooms,
+        "baths": baths,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
 
     return render(request, "rooms/search.html", context={**form, **choices})
