ac938d9c6bb53087beed0d0b3267f081d688cdb4
sewookJung <tpdnrqkqh@naver.com>
2021-08-18 16:33:08 +0900

    [ADD] Filtering of room by form condition

--- rooms/views.py
+++ rooms/views.py
@@ -33,8 +33,8 @@ def search(request):
     baths = int(request.GET.get("baths", 0))
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("superhost", False)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
 
     form = {
         "city": city,
@@ -48,7 +48,7 @@ def search(request):
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
     }
 
     room_types = models.RoomType.objects.all()
@@ -72,7 +72,36 @@ def search(request):
     if room_type != 0:
         filter_args["room_type__pk"] = room_type
 
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if instant is True:
+        filter_args["instant_book"] = instant
+
+    if superhost is True:
+        filter_args["host__superhost"] = superhost
+
     rooms = models.Room.objects.filter(**filter_args)
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            rooms = rooms.filter(amenities__pk=int(s_amenity))
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            rooms = rooms.filter(amenities__pk=int(s_facility))
 
     return render(
         request, "rooms/search.html", context={**form, **choices, "rooms": rooms}
