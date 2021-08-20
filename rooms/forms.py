439acdc1b75123adc3d0c12ca38d14c321fd1386
sewookJung <tpdnrqkqh@naver.com>
2021-08-20 14:25:44 +0900

    [ADD] Seme forms element in form class

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,8 +1,24 @@
 from django import forms
+from django_countries.fields import CountryField
 from . import models
 
 
 class SearchForm(forms.Form):
     city = forms.CharField(initial="Anywhere")
+    country = CountryField(default="KR").formfield()
+    room_type = forms.ModelChoiceField(
+        required=False, empty_label="Any Kind", queryset=models.RoomType.objects.all()
+    )
     price = forms.IntegerField(required=False)
-    room_type = forms.ModelChoiceField(queryset=models.RoomType.objects.all())
+    guests = forms.IntegerField(required=False)
+    beds = forms.IntegerField(required=False)
+    baths = forms.IntegerField(required=False)
+    bedrooms = forms.IntegerField(required=False)
+    Superhost = forms.BooleanField(required=False)
+    Instant_book = forms.BooleanField(required=False)
+    Amenities = forms.ModelMultipleChoiceField(
+        models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple()
+    )
+    Facilities = forms.ModelMultipleChoiceField(
+        models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple()
+    )
