3dd8e547f7e5847806587b20ce3b33a52c1bc80c
sewookJung <tpdnrqkqh@naver.com>
2021-08-24 23:25:12 +0900

    [ADD] If form valid is true create user and login logic in form valid method

--- users/views.py
+++ users/views.py
@@ -29,6 +29,7 @@ def log_out(request):
 
 
 class SignUpView(FormView):
+
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
@@ -37,3 +38,14 @@ class SignUpView(FormView):
         "last_name": "Jung",
         "email": "tpdnrqkqh@naver.com",
     }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+
+        if user is not None:
+            login(self.request, user)
+
+        return super().form_invalid(form)
